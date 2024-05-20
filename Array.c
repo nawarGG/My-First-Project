0f7246973b571a4798315945d10f052db081fd5f
Nawar Zannah <147175357+nawarGG@users.noreply.github.com>
2024-05-20 15:49:41 +0600

    Add files via upload

---
+++ Array.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+
+int main(){
+
+    int n;
+    scanf("%d",&n);
+
+    int ara[n];
+    for(int i =0;i<n;i++)
+        scanf("%d",&ara[i]);
+
+    //find an element - linear search
+
+
+    //k-th max element
+    int k;
+    scanf("%d",&k);
+    if(k>n){
+        printf("invalid input\n");
+        return 0;
+    }
+
+    for(int i =0;i<n;i++)
+    {
+        int flag = 0;
+        for(int j = 0; j<n-1-i;j++){
+            if(ara[j]>ara[j+1]){
+                int temp = ara[j];
+                ara[j] = ara[j+1];
+                ara[j+1] = temp;
+                flag++;
+            }
+        }
+        if(flag==0){
+            break;
+        }
+    }
+
+    printf("%d-th max element is :%d\n",k,ara[n-k]);
+
+}
